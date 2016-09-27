44e195b0f5f2c649f5243c17400810e0786a3b56
novneetnov <novneet346iith@gmail.com>
2016-09-27 22:15:43 +0530

    Implemented Creation Process for posts and refractored code

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -12,4 +12,22 @@ describe 'navigate' do
 			expect(page).to have_content(/Posts/)
 		end
 	end
+
+	describe 'Creation' do
+		before do
+			visit new_post_path
+		end
+		it 'has a new form that can be reached' do
+			expect(page.status_code).to eq(200)
+		end
+
+		it 'can be created from new form page' do
+			fill_in 'post[date]', with: Date.today
+			fill_in 'post[rationale]', with: "Some Rationale"
+			
+			click_on 'Save'
+			
+			expect(page).to have_content("Some Rationale")
+		end
+	end
 end
