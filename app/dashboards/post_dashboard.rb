f25176c6bd5cc8e593ab2c1680a988b75bfae974
novneetnov <novneet346iith@gmail.com>
2016-10-13 17:20:01 +0530

    Customized search in Admin, User and Post Dashboard

--- app/dashboards/post_dashboard.rb
+++ app/dashboards/post_dashboard.rb
@@ -11,7 +11,7 @@ class PostDashboard < Administrate::BaseDashboard
     user: Field::BelongsTo,
     id: Field::Number,
     date: Field::DateTime,
-    rationale: Field::Text,
+    rationale: Field::Text.with_options(searchable: true),
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
   }.freeze
@@ -23,7 +23,6 @@ class PostDashboard < Administrate::BaseDashboard
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
     :user,
-    :id,
     :date,
     :rationale,
   ].freeze
@@ -32,7 +31,6 @@ class PostDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
     :user,
-    :id,
     :date,
     :rationale,
     :created_at,
