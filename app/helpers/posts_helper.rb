ca3da30b9f1f51a575a164c0b766513a580de555
novneetnov <novneet346iith@gmail.com>
2016-10-18 17:51:51 +0530

    Implement labels for status in Post#index

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,10 @@
 module PostsHelper
+	def status_label(status)
+		status_span_generator(status)
+	end
+
+	def status_span_generator(status)
+		labels_hash = { "submitted": "primary", "approved": "success", "rejected": "danger" }
+		content_tag(:span, status.titleize, class: "label label-#{labels_hash[status.to_sym]}")
+	end
 end
