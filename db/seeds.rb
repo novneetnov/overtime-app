36f78b43d807fe0276339acc89d01bf2864d1ea1
novneetnov <novneet346iith@gmail.com>
2016-10-25 15:16:10 +0530

    updated the Audit Log Factory

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,4 +28,10 @@ puts "1 Admin User Created"
 							 overtime_request: 2.5)
 end
 
-puts "100 Posts have been created."
+100.times do |audit_log|
+	AuditLog.create!(user_id: User.last.id, 
+									 status: 0, 
+									 start_date: (Date.today - 6.days)) 
+end
+
+puts "100 AuditLogs have been created."
