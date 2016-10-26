1fbcd64e7433f5185e41ae360acc925b45f24c0c
novneetnov <novneet346iith@gmail.com>
2016-10-26 13:43:07 +0530

    Add status enum to AuditLog and integrate style helper for status in views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,20 @@ module ApplicationHelper
 	def active?(path)
 		"active" if current_page?(path)
 	end
+
+	def status_label(status)
+		status_span_generator(status)
+	end
+
+	def status_span_generator(status)
+		labels_hash = { 
+			"submitted": "primary", 
+			"approved": "success", 
+			"rejected": "danger",
+			"pending": "primary",
+			"confirmed": "success"
+	 	}
+		content_tag(:span, status.titleize, class: "label label-#{labels_hash[status.to_sym]}")
+	end
+
 end
