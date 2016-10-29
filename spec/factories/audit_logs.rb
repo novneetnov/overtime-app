69fcba63ee6ddc1f5b1f24f8922d1b5f9a0f4b1a
novneetnov <novneet346iith@gmail.com>
2016-10-29 11:48:31 +0530

    Build Functional Buttons for Employees to confirm Audit Log status from Homepage

--- spec/factories/audit_logs.rb
+++ spec/factories/audit_logs.rb
@@ -1,14 +1,20 @@
 FactoryGirl.define do
-  factory :audit_log do
-    user
-    status 0
-    start_date (Date.today - 6.days)
-    end_date nil
-  end
-	
+	factory :audit_log do
+		user
+		status 0
+		start_date (Date.today - 6.days)
+		end_date nil
+	end
+
 	factory :audit_log_without_status_and_start_date, class: "AuditLog" do
-    user
-    end_date nil
-  end
+		user
+		end_date nil
+	end
+
+	factory :audit_log_without_user, class: "AuditLog" do
+		status 0
+		start_date (Date.today - 6.days)
+		end_date nil
+	end
 
 end
--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -12,4 +12,16 @@ describe 'Homepage' do
 		expect(current_path).to eq post_path(post)
 		expect(post.reload.status).to eq("approved")
 	end
+
+	it 'allows the Employee to change Audit Logs status from the Homepage' do
+		user = FactoryGirl.create(:user)
+		audit_log = FactoryGirl.create(:audit_log_without_user, user_id: user.id)
+		login_as(user, scope: :user)
+
+		visit root_path
+		click_link "confirm_#{audit_log.id}"
+
+		expect(current_path).to eq root_path(audit_log)
+		expect(audit_log.reload.status).to eq("confirmed")
+	end
 end
