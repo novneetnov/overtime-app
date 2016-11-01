5e78600d5695ecfe6550f403d7d5f2bbb81ec986
novneetnov <novneet346iith@gmail.com>
2016-11-01 18:17:56 +0530

    Move mail_setup to initializers folder and test sendgrid in dev and prod

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,15 +18,6 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 	config.action_mailer.perform_deliveries = true
 	config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-	config.action_mailer.smtp_settings = {
-		:user_name => ENV['GMAIL_USERNAME'],
-		:password => ENV['GMAIL_PASSWORD'],
-		:domain => 'gmail.com',
-		:address => 'smtp.gmail.com',
-		:port => 587,
-		:authentication => :plain,
-		:enable_starttls_auto => true
-	}
 	#config.action_mailer.default_options = {from: 'no-reply@example.com'}
 
   # Print deprecation notices to the Rails logger.
