d054d0d9397a708d3df7468772ecb98c15bf7cd2
novneetnov <novneet346iith@gmail.com>
2016-11-02 19:52:46 +0530

    Added Update callbacks to set confirm date

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -5,9 +5,13 @@ class AuditLog < ActiveRecord::Base
 	validates :status, :start_date, presence: true
 
 	after_initialize :set_defaults
-	
+	before_update :set_end_date, if: :confirmed?
+
 	private
-	
+	def set_end_date
+		self.end_date = Date.today
+	end
+
 	# When the Audit Logs are run via rake task on Sundays, the start date gets set to the previous Mondays.
 	def set_defaults
 		self.start_date ||= 6.days.ago
