d7e1229debc562c2e7eaded1d5bc09d5d566ea7a
novneetnov <novneet346iith@gmail.com>
2016-11-27 17:32:54 +0530

    Add dummy admin and employee login in the signin page

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ User.delete_all
 
 #Post.skip_callback(:save, :after, :update_audit_log)
 
-@employee = Employee.create!(
+@employee1 = Employee.create!(
 	email: "novneet@iith.ac.in", 
 	password: "password", 
 	password_confirmation: "password", 
@@ -14,10 +14,19 @@ User.delete_all
 	ssn: 1234,
 	company: "ABC"
 )
+@employee2 = Employee.create!(
+	email: "employee@example.com", 
+	password: "password", 
+	password_confirmation: "password", 
+	first_name: "Jon", 
+	last_name: "Snow",
+	phone: "9876543210",
+	ssn: 1234,
+	company: "ABC"
+)
+puts "2 User created"
 
-puts "1 User created"
-
-@admin = AdminUser.create!(
+@admin1 = AdminUser.create!(
 	email: "novneet346iith@gmail.com", 
 	password: "password", 
 	password_confirmation: "password", 
@@ -27,29 +36,54 @@ puts "1 User created"
 	ssn: 1234,
 	company: "ABC"
 )
+@admin2 = AdminUser.create!(
+	email: "admin@example.com", 
+	password: "password", 
+	password_confirmation: "password", 
+	first_name: "Jon", 
+	last_name: "Snow",
+	phone: "1234567890",
+	ssn: 1234,
+	company: "ABC"
+)
 
-puts "1 Admin User Created"
+puts "2 Admin User Created"
 
 4.times do |n|
 	AuditLog.create!(
-		user_id: @employee.id,
+		user_id: @employee1.id,
 		status: 0, 
 		start_date: (Date.today - (7*n+6).days)
 	) 
 end
-
-puts "4 AuditLogs created."
+4.times do |n|
+	AuditLog.create!(
+		user_id: @employee2.id,
+		status: 0, 
+		start_date: (Date.today - (7*n+6).days)
+	) 
+end
+puts "4 AuditLogs of each employee created."
 
 3.times do |n|
 	Post.create!(
 		date: Date.today - (7*n+7).days, 
 		#work_performed: "#{post} rationale content", 
 		work_performed: "Ipsum praesentium adipisci dolore tenetur sint alias maiores molestiae sequi nam? Sint nemo architecto cumque reprehenderit natus. Esse nobis quidem vero dignissimos delectus. Earum ab error fugit esse id eum!",
-		user_id: @employee.id, 
+		user_id: @employee1.id, 
+		daily_hours: 2.5
+	)
+end
+3.times do |n|
+	Post.create!(
+		date: Date.today - (7*n+7).days, 
+		#work_performed: "#{post} rationale content", 
+		work_performed: "Ipsum praesentium adipisci dolore tenetur sint alias maiores molestiae sequi nam? Sint nemo architecto cumque reprehenderit natus. Esse nobis quidem vero dignissimos delectus. Earum ab error fugit esse id eum!",
+		user_id: @employee2.id, 
 		daily_hours: 2.5
 	)
 end
 
-puts "3 Posts created"
+puts "3 Posts of each employee created"
 
 
